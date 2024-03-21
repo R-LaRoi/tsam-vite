366750869cfc477718833d680eb27b6e14e47875
R. LaRoi <are@raewhoo.com>
2024-03-21 19:15:55 -0400

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,14 +4,14 @@ import './stylesheets/nav.css'
 export default function Navbar() {
   return  (
 <>
-    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" id="nav-btn">TSC <i class="fa-solid fa-bars"></i></button>
+    <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" id="nav-btn">TSC <i className="fa-solid fa-bars"></i></button>
 
-<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title tsc-name" id="offcanvasWithBothOptionsLabel" >Trupti Samuel Consultancy</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+<div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title tsc-name" id="offcanvasWithBothOptionsLabel" >Trupti Samuel Consultancy</h5>
+    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body tsc-name">
+  <div className="offcanvas-body tsc-name">
     <p>Learn more about our consultations. Schedule your appointment today!</p>
 <ul className="">
         <li> <Link to="/" className="nav-list">TSC</Link>  </li>
